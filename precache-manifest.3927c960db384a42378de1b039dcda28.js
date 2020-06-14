c0259780a1f71200b2694bcc4f66f9e84b24d316
z1gonzo <goonsky@gmail.com>
2020-06-14 16:59:42 +0200

    Updates

---
+++ precache-manifest.3927c960db384a42378de1b039dcda28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78e8731674365dfbb942137fd749c5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ddebd3d1e0fd096d378",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b579a758292301bb229",
+    "url": "/static/js/2.4e751510.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e751510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ddebd3d1e0fd096d378",
+    "url": "/static/js/main.6fcec647.chunk.js"
+  },
+  {
+    "revision": "024346d690475a064ca6",
+    "url": "/static/js/runtime-main.c681a8c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
